0241e1a621aacda609d649ffa92b5e0e58724ef9
not-jacooob <notjacooob.rblx@gmail.com>
2026-09-05 00:22:35 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,7 @@
+if (typeof window !== 'undefined') {
+  const script = document.createElement('script');
+  script.src = 'https://eu.fw-cdn.com/13697253/1610601.js';
+  script.setAttribute('chat', 'true');
+  script.async = true;
+  document.head.appendChild(script);
+}
