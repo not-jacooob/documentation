d048a38f733e36f0bb7b459bbeeddaf4ac259095
not-jacooob <notjacooob.rblx@gmail.com>
2026-09-05 16:52:09 +0100

    Update and rename copyright.js to copyright.html

--- copyright.js
+++
@@ -1,6 +0,0 @@
-if (typeof window !== 'undefined') {
-  const meta = document.createElement('meta');
-  meta.name = 'copyrighted-site-verification';
-  meta.content = '0c55d12361c089ff';
-  document.head.appendChild(meta);
-}
