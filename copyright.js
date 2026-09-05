40b133d8120a98b6d1e34e4dfc8069501a803db7
not-jacooob <notjacooob.rblx@gmail.com>
2026-09-05 16:49:49 +0100

    Update and rename chat.js to copyright.js

---
+++ copyright.js
@@ -0,0 +1,6 @@
+if (typeof window !== 'undefined') {
+  const meta = document.createElement('meta');
+  meta.name = 'copyrighted-site-verification';
+  meta.content = '0c55d12361c089ff';
+  document.head.appendChild(meta);
+}
